8409a834d1fb939419d9a109a87fc8c22734b464
Ali Minaei <ali.minaei@gmail.com>
2020-04-05 20:26:33 +0100

    moving file read to a new function
    Moving reading the JSON file to a separate function and testing some edge cases.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import hashlib
 import json
+import os
 
 class Timestamp:
     def __init__(self, op, prefix, postfix):
@@ -14,14 +15,37 @@ Hints: use hashlib.sha256 and hash.hexdigest. message is big-endian while merkle
 def verify_hash(timestamps, message, merkle_root):
     return False
 
+
+def load_timestamp_list_from_file(filename):
+    """
+    This function receives a path to a JSON file that contains the timestamps and returns a list of unvalidated/unparsed timestamps.
+    """
+
+    if not filename:
+        raise ValueError("Invalid File Name.")
+
+    timestamp_list = []
+
+    if not os.path.exists(filename):
+        raise IOError("File Not Found.")
+
+    try:
+        with open(filename, "rt") as fp:
+            timestamp_list = json.load(fp)
+
+    except ValueError:
+        raise ValueError("The file is not a valid JSON format.")
+
+    return timestamp_list
+
 if __name__ == "__main__":
     msg = "b4759e820cb549c53c755e5905c744f73605f8f6437ae7884252a5f204c8c6e6"
     merkle_root = "f832e7458a6140ef22c6bc1743f09610281f66a1b202e7b4d278b83de55ef58c"    
-    with open("./bag/timestamp.json", "rt") as fp:
-        dat = json.load(fp)
-        timestamps = []
+    timestamp_list = load_timestamp_list_from_file("./bag/timestamp.json")
 
-        if verify_hash(timestamps, msg, merkle_root):
-            print("CORRECT!")
-        else:
-            print("INCORRECT!")
+    timestamps = []
+
+    if verify_hash(timestamps, msg, merkle_root):
+        print("CORRECT!")
+    else:
+        print("INCORRECT!")
--- test.py
+++ test.py
@@ -3,6 +3,18 @@ import main
 
 class TestMain(unittest.TestCase):
 
+    def test_load_timestamp_list_from_file(self):
+        with self.assertRaises(ValueError):
+            main.load_timestamp_list_from_file("")
+
+        with self.assertRaises(ValueError):
+            main.load_timestamp_list_from_file("./bag/timestamp2.json")
+
+        with self.assertRaises(IOError):
+            main.load_timestamp_list_from_file("./bag/timestamp3.json")
+
+        self.assertIsInstance(main.load_timestamp_list_from_file("./bag/timestamp.json"), list)
+
     def test_verify_hash(self):
         self.assertFalse(main.verify_hash("", "", ""))
 
