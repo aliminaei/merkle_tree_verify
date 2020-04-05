9dd9c0810bf854fed5fd8056447037621edb9c50
Ali Minaei <ali.minaei@gmail.com>
2020-04-05 23:05:00 +0100

    moved the marshaling logic
    moved the marshaling logic outside the verify function and updated the tests.

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ class Timestamp:
 This function should walk through the timestamps and verify message against merkleRoot
 Hints: use hashlib.sha256 and hash.hexdigest. message is big-endian while merkleRoot is little-endian.
 """ 
-def verify_hash(timestamp_list, message, merkle_root):
+def verify_hash(timestamps, message, merkle_root):
     # check if initial message is in a valid hex format
     if not is_hex_bytes(message):
         raise ValueError("Invalid initail message. Initial message should be in valid hex format.")
@@ -33,13 +33,6 @@ def verify_hash(timestamp_list, message, merkle_root):
         raise ValueError("Invalid merkle root. Merkle root should be in valid hex format.")
 
     merkle_root = change_endian(merkle_root)
-
-    timestamps = []
-    # Iterate through the list and parse Timestamp objects
-    print("Parsing list of timestamps.")
-    for node in timestamp_list:
-        timestamp = parse_timestamp(node)
-        timestamps.append(timestamp)
 
     # Navigating through the timestamps!
     for timestamp in timestamps:
@@ -112,7 +105,14 @@ if __name__ == "__main__":
 
     # read all the timestamps from the JSON file as a list
     print("Reading timestamps from file.")
-    timestamps = load_timestamp_list_from_file("./bag/timestamp.json")
+    timestamp_list = load_timestamp_list_from_file("./bag/timestamp.json")
+
+    timestamps = []
+    # Iterate through the list and parse Timestamp objects
+    print("Parsing list of timestamps.")
+    for node in timestamp_list:
+        timestamp = parse_timestamp(node)
+        timestamps.append(timestamp)
 
     if verify_hash(timestamps, msg, merkle_root):
         print("CORRECT!")
